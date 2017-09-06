80841549125d5d308af2c426c9539df69fb847c5
Kotet <8435623+kotet@users.noreply.github.com>
2017-09-06 09:01:27 +0900

    Merge sort v1

---
+++ source/merge/v1.d
@@ -0,0 +1,47 @@
+module merge.v1;
+
+import test;
+
+mixin test!sort;
+
+void sort(T)(T[] array) nothrow pure
+{
+    array[] = mergeSort(array);
+}
+
+private T[] mergeSort(T)(T[] array) nothrow pure
+{
+    import std.range : empty, front, popFront;
+
+    if (array.length < 2)
+    {
+        return array;
+    }
+    else
+    {
+        auto a = mergeSort(array[0 .. $ / 2]);
+        auto b = mergeSort(array[$ / 2 .. $]);
+        T[] result;
+
+        foreach (i; 0 .. a.length + b.length)
+        {
+            if (a.empty)
+            {
+                result ~= b.front;
+                b.popFront();
+            }
+            else if (b.empty || a.front < b.front)
+            {
+                result ~= a.front;
+                a.popFront();
+            }
+            else
+            {
+                result ~= b.front;
+                b.popFront();
+            }
+        }
+
+        return result;
+    }
+}
