0a9ef66f909f539fb46f752c661b4482a3854901
Kotet <8435623+kotet@users.noreply.github.com>
2017-09-10 11:17:37 +0900

    Heap sort v1
    nogc In-place

---
+++ source/heap/v1.d
@@ -0,0 +1,75 @@
+module heap.v1;
+
+import test;
+
+mixin test!sort;
+
+void sort(T)(T[] array) @nogc nothrow pure
+{
+
+    foreach (i; 0 .. array.length)
+    {
+        upHeap(array[0 .. i + 1]);
+    }
+    foreach_reverse (i; 0 .. array.length)
+    {
+        downHeap(array[0 .. i + 1]);
+    }
+}
+
+void upHeap(T)(T[] heap) @nogc nothrow pure
+{
+    size_t element = heap.length;
+    while (element != 1)
+    {
+        size_t parent = (element) / 2;
+        if (heap[parent - 1] < heap[element - 1])
+        {
+            swap(heap[parent - 1], heap[element - 1]);
+            element = parent;
+        }
+        else
+        {
+            return;
+        }
+    }
+}
+
+void downHeap(T)(T[] heap) @nogc nothrow pure
+{
+    swap(heap[0], heap[$ - 1]);
+    auto newHeap = heap[0 .. $ - 1];
+
+    size_t element = 1;
+    while (element * 2 - 1 < newHeap.length)
+    {
+        size_t bigChild = () {
+            immutable child1 = element * 2;
+            immutable child2 = child1 + 1;
+
+            immutable child2Exists = child2 - 1 < newHeap.length;
+
+            if (child2Exists && newHeap[child1 - 1] < newHeap[child2 - 1])
+                return child2;
+            else
+                return child1;
+        }();
+
+        if (newHeap[element - 1] < newHeap[bigChild - 1])
+        {
+            swap(newHeap[element - 1], newHeap[bigChild - 1]);
+            element = bigChild;
+        }
+        else
+        {
+            return;
+        }
+    }
+}
+
+void swap(T)(ref T a, ref T b) @nogc nothrow pure
+{
+    immutable tmp = a;
+    a = b;
+    b = tmp;
+}
